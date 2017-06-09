d42c9c6fd28656ceba1609f54ac2969bba6d9569
Man-Yat Chu <energy@waterfall.dyn.berkeley.edu>
2017-06-09 11:53:14 -0700

    change config.py to match rainwoodman.

--- examples/run/config.py
+++ examples/run/config.py
@@ -1,7 +1,4 @@
-from astropy import units
 aout = [0.5, 1.0]
 stages = autostages(aout, astart=0.1, N=5)
 unitary = True
-nc = 64
-#cosmology = Planck15.clone(Tcmb0=0)
-cosmology = Planck15.clone(Neff=0)
+nc = 128
