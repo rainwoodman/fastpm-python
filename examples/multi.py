5a4e01377680c2c2953efda95e5f417f98b669ff
Yu Feng <rainwoodman@gmail.com>
2017-09-01 16:38:12 -0700

    Use LinearPower, not EHPower
    This fixes the difference between multi and single species.

--- examples/multi.py
+++ examples/multi.py
@@ -5,7 +5,7 @@ from fastpm.state import Baryon, CDM, NCDM
 from fastpm.core import Solver as Solver
 from fastpm.core import leapfrog
 
-from nbodykit.cosmology import Planck15, EHPower
+from nbodykit.cosmology import Planck15, LinearPower
 from nbodykit.algorithms.fof import FOF
 from nbodykit.algorithms.fftpower import FFTPower
 from nbodykit.source import ArrayCatalog, MultipleSpeciesCatalog
@@ -20,13 +20,14 @@ pm = ParticleMesh(BoxSize=512, Nmesh=[64, 64, 64], dtype='f4', resampler='tsc')
 Q = pm.generate_uniform_particle_grid()
 
 stages = numpy.linspace(0.1, 1.0, 10, endpoint=True)
+#stages = [1.]
 
 solver = Solver(pm, Planck15, B=2)
 solver_multi = SolverMulti(pm, Planck15, B=2)
 
-wn = solver.whitenoise(400)
-dlin = solver.linear(wn, EHPower(Planck15, 0))
-lpt = solver.lpt(dlin, Q, stages[0])
+wn = solver.whitenoise(400, unitary=True)
+dlin = solver.linear(wn, LinearPower(Planck15, 0))
+lpt = solver.lpt(dlin, Q, stages[0], order=2)
 
 def monitor(action, ai, ac, af, state, event):
     if pm.comm.rank == 0:
@@ -38,7 +39,7 @@ def monitor_multi(action, ai, ac, af, state, event):
 
 state1 = solver.nbody(lpt.copy(), leapfrog(stages), monitor=monitor)
 
-tf = get_species_transfer_function_from_class(Planck15, 9)
+tf = get_species_transfer_function_from_class(Planck15, 1.0 / stages[0] - 1)
 
 prm = solver_multi.primordial(wn, Planck15.Primordial.get_pk)
 
@@ -46,7 +47,7 @@ ic = solver_multi.lpt(prm, {
             '0': (Baryon, tf['d_b'], tf['dd_b']),
             '1': (CDM, tf['d_cdm'], tf['dd_cdm']),
             '4': (NCDM, tf['d_ncdm[0]'], tf['dd_ncdm[0]']),
-        }, Q, a=0.1)
+        }, Q, a=stages[0], order=2)
 
 state2 = solver_multi.nbody(ic.copy(), leapfrog(stages), monitor=monitor_multi)
 
@@ -55,10 +56,16 @@ if pm.comm.rank == 0:
 
 cat1 = state1.to_catalog(Nmesh=pm.Nmesh)
 cat2 = state2.to_catalog(Nmesh=pm.Nmesh)
+cat2_cdm = state2['1'].to_catalog(Nmesh=pm.Nmesh)
+cat2_b = state2['0'].to_catalog(Nmesh=pm.Nmesh)
+cat2_ncdm = state2['4'].to_catalog(Nmesh=pm.Nmesh)
 print(cat2.attrs)
 
 r1 = FFTPower(cat1, mode='1d')
 r2 = FFTPower(cat2, mode='1d')
+r2_cdm = FFTPower(cat2_cdm, mode='1d')
+r2_b = FFTPower(cat2_b, mode='1d')
+r2_ncdm = FFTPower(cat2_ncdm, mode='1d')
 
 cat1.save('MULTI/core', ('Position',))
 cat2.save('MULTI/mult', ('0/Position','1/Position', '4/Position'))
@@ -75,6 +82,10 @@ if pm.comm.rank == 0:
 
     ax = fig.add_subplot(111)
 
-    ax.plot(r1.power['k'], r2.power['power'] / r1.power['power'], label='ncdm / core')
-
-    fig.savefig('ncdm-result.png')
+    ax.plot(r1.power['k'], r2.power['power'] / r1.power['power'], label='tot / core')
+    ax.plot(r1.power['k'], r2_b.power['power'] / r1.power['power'], label='b / core')
+    ax.plot(r1.power['k'], r2_cdm.power['power'] / r1.power['power'], label='cdm/ core')
+    ax.plot(r1.power['k'], r2_ncdm.power['power'] / r1.power['power'], label='ncdm / core')
+    ax.legend()
+    ax.set_xscale('log')
+    fig.savefig('multi-result.png')
