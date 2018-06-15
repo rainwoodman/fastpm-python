3e753c0abad73c090b9e898770ed083bd104ceb1
Yu Feng <rainwoodman@gmail.com>
2018-06-14 17:05:14 -0700

    assert solver gives identical result independent of num of ranks.

--- fastpm/tests/test_core.py
+++ fastpm/tests/test_core.py
@@ -60,3 +60,34 @@ def test_lpt():
 
     fac = 1 / (0.01 ** 2 * pt.E(0.01)) * (pt.Gf(1.0) - pt.Gf(0.01)) / pt.gf(0.01)
     assert_allclose(state2.P[...], state1.P[...] + fac * state1.F[...])
+
+from runtests.mpi import MPITest
+@MPITest([1, 2, 3, 4])
+def test_solver_convergence(comm):
+    from mpi4py import MPI
+    pm = ParticleMesh(BoxSize=128., Nmesh=[8, 8, 8], comm=comm)
+    pm_ref = ParticleMesh(BoxSize=128., Nmesh=[8, 8, 8], comm=MPI.COMM_SELF)
+
+    Plin = LinearPower(Planck15, redshift=0, transfer='EisensteinHu')
+
+    Q = pm_ref.generate_uniform_particle_grid(shift=0)
+
+    def solve(pm):
+        solver = Solver(pm, Planck15, B=1)
+        q = numpy.array_split(Q, pm.comm.size)[pm.comm.rank]
+        wn = solver.whitenoise(1234)
+        dlin = solver.linear(wn, lambda k: Plin(k))
+
+        state = solver.lpt(dlin, q, a=0.1, order=2)
+        state = solver.nbody(state, leapfrog([0.1, 0.5, 1.0]))
+
+        d = {}
+        for key in 'X', 'P', 'F':
+            d[key] = numpy.concatenate(pm.comm.allgather(getattr(state, key)), axis=0)
+        return d
+
+    s = solve(pm)
+    r = solve(pm_ref)
+    assert_allclose(s['X'], r['X'])
+    assert_allclose(s['P'], r['P'])
+    assert_allclose(s['F'], r['F'])
